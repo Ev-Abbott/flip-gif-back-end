35a9c13c38b9d08868d85eab1e9ff88755bcb0ac
abbotteverett <severettabbott@gmail.com>
2018-04-04 11:35:50 -0700

    Added server boilerplate

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Sets up enviroment variables for config
+require('dotenv').config();
+
+app.use(cors({ exposedHeaders: 'Auth' }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+// Include routes here
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    console.log(err);
+    res.status(status).json({ error: err });
+});
+
+app.use((req, res, next) => {
+    res.status(404).json({ error: { message: 'Route not found!' }});
+});
+
+function listener() {
+    console.log(`Listening on port ${port}...`);
+}
+
+app.listen(port, listener);
